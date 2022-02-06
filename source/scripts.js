acac1a65ecb15b8120d034a9e733581b0c7d7c5e
Gintare Cerniauskaite <gintare.cerniauskaite@protonmail.com>
2022-02-06 15:46:11 +0200

    Use Intersection Observer API if it is available

--- source/scripts.js
+++ source/scripts.js
@@ -49,74 +49,6 @@ techIcons.forEach((techIcon) => {
   techIcon.addEventListener('mouseout', toggleDevIcon);
 });
 
-function comesInViewport(element, idx = 0) {
-  const rect = element.getBoundingClientRect();
-  return (
-    rect.top >=
-      -0.7 * (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.top <=
-      0.7 * (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.left >= 0 &&
-    rect.right <=
-      (window.innerWidth || document.documentElement.clientWidth) + 100
-  );
-}
-
-document.addEventListener(
-  'scroll',
-  function () {
-    // checking if tech stack comes in viewport
-    const el = document.querySelector('.tech-stack');
-    const divs = document.querySelectorAll('.tech-stack__list i');
-    if (comesInViewport(el, 0)) {
-      for (var i = 0; i < divs.length; i++) {
-        divs[i].classList.add('active');
-      }
-    } else {
-      for (var i = 0; i < divs.length; i++) {
-        divs[i].classList.remove('active');
-      }
-    }
-    //checking if project comes in viewport
-    const projectArray = Array.from(document.querySelectorAll('.project'));
-
-    projectArray.forEach((project, idx) => {
-      toggleActiveClass(project, idx);
-    });
-
-    function toggleActiveClass(element, idx) {
-      if (comesInViewport(element, idx)) {
-        element.classList.add('active');
-        const target = element
-          .querySelector('.project__wrapper')
-          .querySelector('.project__info')
-          .querySelector('.project__icons')
-          .querySelector('.fa-chevron-up');
-        if (target) {
-          target.classList.remove('fa-chevron-up');
-          target.classList.add('fa-chevron-down');
-        }
-      } else {
-        element.classList.remove('active');
-      }
-    }
-
-    // checking if title is in viewport
-    const title = document.querySelector('.about__wrapper');
-    const titles = Array.from(document.querySelector('.about__title'));
-    titles.forEach((title) => {
-      if (comesInViewport(title)) {
-        title.classList.remove('paused');
-      } else {
-        title.classList.add('paused');
-      }
-    });
-  },
-  {
-    passive: true,
-  }
-);
-
 function toggleInfo(e) {
   e.stopPropagation();
   const target = e.target;
@@ -171,3 +103,142 @@ const homeBtn = document.querySelector('.header__brand');
 homeBtn.addEventListener('click', () => {
   window.scrollTo(0, 0);
 });
+
+const arrow = document.querySelector('.arrow-down');
+arrow.addEventListener('click', function () {
+  const target = document.querySelector('.tech-stack');
+  window.scrollTo(0, target.offsetTop);
+});
+if (!!window.IntersectionObserver) {
+  //intersection
+  const options = {
+    rootMargin: '0px',
+    threshold: [0.3, 0.6],
+  };
+  const callback = (entries) => {
+    const about = document.querySelector('.about');
+    const tech = document.querySelector('.tech-stack');
+
+    entries.forEach((entry) => {
+      if (entry.target === tech) {
+        // animate tech stacking whenever comes in viewport
+        const divs = document.querySelectorAll('.tech-stack__list i');
+        if (entry.isIntersecting) {
+          for (let i = 0; i < divs.length; i++) {
+            divs[i].classList.add('active');
+          }
+        } else {
+          for (let i = 0; i < divs.length; i++) {
+            divs[i].classList.remove('active');
+          }
+        }
+      } else if (entry.target === about) {
+        // pause title animation if not in viewport
+        const titles = Array.from(document.querySelectorAll('.about__title'));
+        if (entry.isIntersecting) {
+          titles.forEach((title) => {
+            title.classList.remove('paused');
+          });
+        } else {
+          titles.forEach((title) => {
+            title.classList.add('paused');
+          });
+        }
+      } else {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('active');
+          const chevron = entry.target
+            .querySelector('.project__wrapper')
+            .querySelector('.project__info')
+            .querySelector('.project__icons')
+            .querySelector('.fa-chevron-up');
+          if (chevron) {
+            chevron.classList.remove('fa-chevron-up');
+            chevron.classList.add('fa-chevron-down');
+          }
+        } else {
+          entry.target.classList.remove('active');
+        }
+      }
+    });
+  };
+
+  const sections = [
+    document.querySelector('.about'),
+    document.querySelector('.tech-stack'),
+    ...Array.from(document.querySelectorAll('.project')),
+  ];
+  const observer = new IntersectionObserver(callback, options);
+
+  sections.forEach((section, index) => {
+    observer.observe(section);
+  });
+} else {
+  function comesInViewport(element, idx = 0) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >=
+        -0.7 * (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <=
+        0.7 * (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.left >= 0 &&
+      rect.right <=
+        (window.innerWidth || document.documentElement.clientWidth) + 100
+    );
+  }
+
+  document.addEventListener(
+    'scroll',
+    function () {
+      // checking if tech stack comes in viewport
+      const el = document.querySelector('.tech-stack');
+      const divs = document.querySelectorAll('.tech-stack__list i');
+      if (comesInViewport(el, 0)) {
+        for (var i = 0; i < divs.length; i++) {
+          divs[i].classList.add('active');
+        }
+      } else {
+        for (var i = 0; i < divs.length; i++) {
+          divs[i].classList.remove('active');
+        }
+      }
+      //checking if project comes in viewport
+      const projectArray = Array.from(document.querySelectorAll('.project'));
+
+      projectArray.forEach((project, idx) => {
+        toggleActiveClass(project, idx);
+      });
+
+      function toggleActiveClass(element, idx) {
+        if (comesInViewport(element, idx)) {
+          element.classList.add('active');
+          const target = element
+            .querySelector('.project__wrapper')
+            .querySelector('.project__info')
+            .querySelector('.project__icons')
+            .querySelector('.fa-chevron-up');
+          if (target) {
+            target.classList.remove('fa-chevron-up');
+            target.classList.add('fa-chevron-down');
+          }
+        } else {
+          element.classList.remove('active');
+        }
+      }
+
+      // checking if title is in viewport
+      const title = document.querySelector('.about__wrapper');
+      const titles = Array.from(document.querySelector('.about__title'));
+      titles.forEach((title) => {
+        if (comesInViewport(title)) {
+          title.classList.remove('paused');
+        } else {
+          title.classList.add('paused');
+        }
+      });
+    },
+    {
+      passive: true,
+    }
+  );
+}
